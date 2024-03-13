d5b3877995ab3ccb34320820f676b53f11e0f3c9
MsalgadoJ <mayra.rinconess@gmail.com>
2024-03-13 19:25:01 +0100

    added absolute imports and update vite config to read them

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+// for reading absolute imports
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/setupTests.ts",
-    include: ["**/tests/**/*.test.[tj]sx"],
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    include: ['**/tests/**/*.test.[tj]sx'],
     css: true,
   },
 });
