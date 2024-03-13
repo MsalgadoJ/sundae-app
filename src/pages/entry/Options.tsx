aecca92f79ce95c52ab025712b1f9c7205cb68bf
MsalgadoJ <mayra.rinconess@gmail.com>
2024-03-13 19:06:14 +0100

    implemented context and added it to the tests

--- src/pages/entry/Options.tsx
+++ src/pages/entry/Options.tsx
@@ -1,23 +1,36 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import Row from "react-bootstrap/Row";
-import ScoopOption from "./ScoopOption";
-import ToppingOption from "./ToppingOption";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Row from 'react-bootstrap/Row';
+import ScoopOption from './ScoopOption';
+import ToppingOption from './ToppingOption';
+import AlertBanner from '../common/AlertBanner';
+import { pricePerItem } from '@/constants/index';
+import { formatCurrency } from '@/utilities';
+import { OptionType, useOrderDetails } from '@/contexts/OrderDetails';
 
-export default function Options({ optionType }) {
+interface OptionsProps {
+  optionType: OptionType;
+}
+
+export default function Options({ optionType }: OptionsProps) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
+  const { totals } = useOrderDetails();
 
-  // optionType is 'scoops' or 'toppings
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
-      .catch((error) => {
-        // TODO: handle error response
-      });
+      .catch(() => setError(true));
   }, [optionType]);
 
-  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
+  if (error) {
+    // @ts-ignore
+    return <AlertBanner />;
+  }
+
+  const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
+  const title = optionType[0].toUpperCase() + optionType.slice(1).toLowerCase();
 
   const optionItems = items.map((item) => (
     <ItemComponent
@@ -27,5 +40,14 @@ export default function Options({ optionType }) {
     />
   ));
 
-  return <Row>{optionItems}</Row>;
+  return (
+    <>
+      <h2>{title}</h2>
+      <p>{formatCurrency(pricePerItem[optionType])} each</p>
+      <p>
+        {title} total: {formatCurrency(totals[optionType])}
+      </p>
+      <Row>{optionItems}</Row>
+    </>
+  );
 }
--- src/pages/entry/tests/Options.test.tsx
+++ src/pages/entry/tests/Options.test.tsx
@@ -1,25 +1,25 @@
-import { test, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
-import Options from "../Options";
+import { test, expect } from 'vitest';
+import { render, screen } from '@/test-utils/testing-library-utils';
+import Options from '../Options';
 
-test("displays image for each scoop option from server", async () => {
+test('displays image for each scoop option from server', async () => {
   render(<Options optionType="scoops" />);
 
   // find images
-  const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
+  const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i });
   expect(scoopImages).toHaveLength(2);
 
   // confirm alt text of images
   // @ts-ignore
   const altText = scoopImages.map((element) => element.alt);
-  expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+  expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
 });
 
-test("displays image for each topping from server", async () => {
+test('displays image for each topping from server', async () => {
   render(<Options optionType="toppings" />);
 
   // find images
-  const toppingImages = await screen.findAllByRole("img", {
+  const toppingImages = await screen.findAllByRole('img', {
     name: /topping$/i,
   });
   expect(toppingImages).toHaveLength(3);
@@ -28,8 +28,8 @@ test("displays image for each topping from server", async () => {
   // @ts-ignore
   const altText = toppingImages.map((el) => el.alt);
   expect(altText).toEqual([
-    "Cherries topping",
-    "M&Ms topping",
-    "Hot fudge topping",
+    'Cherries topping',
+    'M&Ms topping',
+    'Hot fudge topping',
   ]);
 });
